6406419796b3d2c69baefcba0babf027256ab1fe
GregJacobs82 <greg@2book.com>
2019-05-21 11:45:11 -0400

    Create Node & Collection
    Still need to create node inception with total employee count

---
+++ src/components/Node.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="node active">
+        <div class="node__header">
+            {{ team }}
+        </div>
+        <div class="node__title">
+            <div class="node__title--name">
+                {{ name }} {{ id }}
+            </div>
+            <div class="node__title--position">
+                {{ designation }}
+            </div>
+        </div>
+        <div class="node__content">
+            <div class="content1">
+                <div>Immediate Employees</div>
+                <div>{{ employees.length }}</div>
+            </div>
+            <div class="content2">
+                Total Employees
+            </div>
+            <div class="content3">
+                ( + )
+            </div>
+        </div>
+        <div class="node__footer">
+            <div class="footer1">
+                [Add]
+            </div>
+            <div class="footer2">
+                [Edit]
+            </div>
+            <div class="footer3">
+                [Del]
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            id: {
+                type: Number,
+                default: null,
+            },
+            team: {
+                type: String,
+                required: true,
+            },
+            name: {
+                type: String,
+                required: true,
+            },
+            designation: {
+                type: String,
+                required: true,
+            },
+            employees: {
+                type: Array,
+                default() {
+                    return [];
+                },
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    //SIZES
+    $height: 40px;
+
+    //COLORS
+    $color-border: #ddd;
+    $color-bg: #f6f6f6;
+
+    //SHADOWS
+    $shadow-1: #{0 4px 8px -4px rgba(0, 0, 0, .3)};
+    $shadow-2: #{0px 2px 8px rgba(0,0,0,.3)};
+    $shadow-3: #{0px 1px 2px rgba(0,0,0,.3)};
+    $shadow-4: #{0 30px 50px -15px rgba(0,0,0,.35)};
+
+    //*** NODE ***/
+    .node {
+        width: 400px;
+        background:$color-bg;
+        border:1px solid $color-border;
+        margin: 0 auto;
+        box-shadow: $shadow-1;
+        line-height: 1.5em;
+
+        //*** HEADER ***/
+        &__header {
+            background: white;
+            min-height: $height;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: .5em;
+            font-size: 1.25em;
+        }
+
+        //*** TITLE ***/
+        &__title {
+            background: white;
+            min-height: $height;
+            padding: 1em .5em;
+            border-bottom: 1px solid $color-border;
+            &--name {
+                font-size: 1.25em;
+            }
+            &--position {
+                color: #888;
+            }
+        }
+
+        //*** CONTENT ***/
+        &__content,
+        &__footer {
+            background: white;
+            padding: 1em .5em;
+            display: flex;
+            align-items:center;
+            justify-content: space-evenly;
+                border-bottom: 1px solid $color-border;
+        }
+
+        //*** NODE ACTIVE STATUS STYLES ***/
+        &.active {
+            & .node__header {
+                background: teal;
+                color: white;
+            }
+        }
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,215 @@
 <template>
     <div class="home">
+        <div
+            class="row"
+            v-for="employee in collection"
+            :key="employee.id"
+        >
+            <Node
+                :id="employee.id"
+                :team="employee.team"
+                :name="employee.name"
+                :designation="employee.designation"
+                :employees="employee.employees"
+            />
+        </div>
     </div>
 </template>
 
 <script>
+    // @ is an alias to /src
+    import Node from '@/components/Node.vue'
+
+    export default {
+        name: 'home',
+        components: {
+            Node
+        },
+        data () {
+            return {
+                collection: [
+
+                    // - Employee1
+                    {
+                        id: 1,
+                        team: "Tech",
+                        name: "Employee Name",
+                        designation: "Software Engineer",
+                        employees: [
+                            // - Employee11
+                            {
+                                id: 11,
+                                team: "Tech",
+                                name: "Employee Name",
+                                designation: "Software Engineer",
+                                employees: [
+                                    // - Employee111
+                                    {
+                                        id: 111,
+                                        team: "Tech",
+                                        name: "Employee Name",
+                                        designation: "Software Engineer",
+                                        employees: [
+                                            // - Employee1111
+                                            {
+                                                id: 1111,
+                                                team: "Tech",
+                                                name: "Employee Name",
+                                                designation: "Software Engineer",
+                                                employees: [
+                                                    // - Employee11111
+                                                    {
+                                                        id: 11111,
+                                                        team: "Tech",
+                                                        name: "Employee Name",
+                                                        designation: "Software Engineer",
+                                                    },
+                                                ],
+                                            },
+                                        ],
+                                    },
+                                ],
+                            },
+
+                            // - Employee12
+                            {
+                                id: 12,
+                                team: "Tech",
+                                name: "Employee Name",
+                                designation: "Software Engineer",
+                            },
+
+                            // - Employee13
+                            {
+                                id: 13,
+                                team: "Tech",
+                                name: "Employee Name",
+                                designation: "Software Engineer",
+                                employees: [
+                                    // - Employee131
+                                    {
+                                        id: 131,
+                                        team: "Tech",
+                                        name: "Employee Name",
+                                        designation: "Software Engineer",
+                                        employees: [
+                                            // - Employee1311
+                                            {
+                                                id: 1311,
+                                                team: "Tech",
+                                                name: "Employee Name",
+                                                designation: "Software Engineer",
+                                            },
+                                        ],
+                                    },
+                                    // - Employee132
+                                    {
+                                        id: 132,
+                                        team: "Tech",
+                                        name: "Employee Name",
+                                        designation: "Software Engineer",
+                                    },
+                                ],
+                            },
+
+                            // - Employee14
+                            {
+                                id: 14,
+                                team: "Tech",
+                                name: "Employee Name",
+                                designation: "Software Engineer",
+                                employees: [
+                                    // - Employee141
+                                    {
+                                        id: 141,
+                                        team: "Tech",
+                                        name: "Employee Name",
+                                        designation: "Software Engineer",
+                                    },
+                                    // - Employee142
+                                    {
+                                        id: 142,
+                                        team: "Tech",
+                                        name: "Employee Name",
+                                        designation: "Software Engineer",
+                                    },
+                                ],
+                            },
+
+                            // - Employee15
+                            {
+                                id: 15,
+                                team: "Tech",
+                                name: "Employee Name",
+                                designation: "Software Engineer",
+                            },
+
+                            // - Employee16
+                            {
+                                id: 16,
+                                team: "Tech",
+                                name: "Employee Name",
+                                designation: "Software Engineer",
+                            },
+                        ],
+                    },
+
+                    // - Employee2
+                    {
+                        id: 2,
+                        team: "Tech",
+                        name: "Employee Name",
+                        designation: "Software Engineer",
+                    },
+
+                    // - Employee3
+                    {
+                        id: 3,
+                        team: "Tech",
+                        name: "Employee Name",
+                        designation: "Software Engineer",
+                        employees: [
+                            // - Employee31
+                            {
+                                id: 31,
+                                team: "Tech",
+                                name: "Employee Name",
+                                designation: "Software Engineer",
+                            },
+
+                            // - Employee32
+                            {
+                                id: 32,
+                                team: "Tech",
+                                name: "Employee Name",
+                                designation: "Software Engineer",
+                                employees: [
+                                    // - Employee321
+                                    {
+                                        id: 321,
+                                        team: "Tech",
+                                        name: "Employee Name",
+                                        designation: "Software Engineer",
+                                    },
+                                ]
+                            },
+
+                            // - Employee33
+                            {
+                                id: 33,
+                                team: "Tech",
+                                name: "Employee Name",
+                                designation: "Software Engineer",
+                            },
+                        ],
+                    },
+                ]
+            }
+        }
+    }
 </script>
+<style scoped lang="scss">
+    .row {
+        margin-bottom: 2em;
+    }
+</style>
