00951c131af26a475267b4f16330b1044d3d0340
GregJacobs82 <greg@2book.com>
2019-06-07 12:48:15 -0400

    Update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
         <h1>Tree Nodes</h1>
         <h3>by Greg Jacobs</h3>
         <p>May 22, 2019</p>
-        <p>This was built as an assignment for Anatta Design of Charleston, SC.</p>
+        <p>This was built to show an example of using recursive components in Vue.</p>
         <p>NOTE: The overall size of elements is scalable to the font-size of the body. Try adjusting your browser default font-size to a smaller or larger size to watch it scale.</p>
     </div>
 </template>
